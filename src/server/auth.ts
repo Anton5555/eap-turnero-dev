5559f7b3bb0d3fc6f56d370fb198277170290a39
Anton5555 <antonmartinezq@gmail.com>
2024-03-26 16:11:17 -0300

    fix: next-auth removed boilerplate + credentialProvider config

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -3,9 +3,9 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 
-import { env } from "~/env";
+import CredentialsProvider from "next-auth/providers/credentials";
+import login from "~/app/api/auth";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -44,19 +44,31 @@ export const authOptions: NextAuthOptions = {
     }),
   },
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" }
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        const userData =  await login({email: credentials.email, password: credentials.password});
+
+        console.log(userData)
+        if (!userData) return null;
+
+        return {
+          id: userData.id,
+          email: userData.mail,
+          name: userData.name,
+        };
+  
+      }
+    })
+  
   ],
 };
 
