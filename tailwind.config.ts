6818094d0fa104e10202e1d358eb7da848226ef7
Anton5555 <antonmartinezq@gmail.com>
2024-03-26 09:19:27 -0300

    brand colors config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,20 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        green: '#009D7D',
+        orange: '#FC5859',
+        cream: '#FEF7F7',
+        black: '#1C1C1C',
+        grey40: '#999999',
+        grey20: '#00000033',
+        grey10: '#0000001A',
+        grey5: '#F2F2F2',
+        white40: '#FFFFFF66',
+        white20: '#FFFFFF33',
+        white10: '#FFFFFF1A',
+        white5: '#FFFFFF0D'
+      }
     },
   },
   plugins: [],
