9b44f2dad71d83694689b2c84be51b8b461dcc9b
Anton5555 <antonmartinezq@gmail.com>
2024-03-28 12:15:43 -0300

    fix: login api call + error handling

--- src/app/api/auth.ts
+++ src/app/api/auth.ts
@@ -1,14 +1,29 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+// Adapter to take the email and password and change them to Username and Password
+const LoginAdapter = (credentials: { email: string; password: string }) => {
+  return {
+    Username: credentials.email,
+    Password: credentials.password,
+  };
+}
+
 const login = async (data: { email: string; password: string }) => {
-  const response = await fetch("/api/login/authenticate", {
+  const response = await fetch(`${API_URL}/login/authenticate`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(LoginAdapter(data)),
   });
 
   if (!response.ok) {
-    throw new Error("Error al iniciar sesión");
+    if (response.status === 401)
+      throw new Error("Contraseña incorrecta");
+    else if (response.status === 404)
+      throw new Error("Usuario no encontrado");
+    else
+      throw new Error("Error al iniciar sesión");
   }
 
   return response.json();
