043eb6671c82e4e3aad7668776ed346c54671437
Anton5555 <antonmartinezq@gmail.com>
2024-04-04 11:09:03 -0300

    refactor: replaced radix-ui popover with headlessui menu

--- src/_components/shared/Profile.tsx
+++ src/_components/shared/Profile.tsx
@@ -1,59 +1,70 @@
 "use client";
 
 import { signOut, useSession } from "next-auth/react";
-import React from "react";
-import { Popover, PopoverContent, PopoverTrigger } from "../common/Popover";
+import React, { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
 import Image from "next/image";
 import ChevronIcon from "../icons/Chevron";
-import Link from "next/link";
 import { Button } from "../common/Button";
 import UserIcon from "../icons/User";
 
 const Profile = () => {
-  const [popoverOpen, setPopoverOpen] = React.useState(false);
-
   const { data: session } = useSession();
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <div className="flex items-center gap-4">
-          <span className="sr-only">Open user menu</span>
+    <Menu as="div" className="relative">
+      {({ open }) => (
+        <>
+          <Menu.Button className="-m-1.5 flex items-center gap-x-4 p-1.5">
+            <span className="sr-only">Open user menu</span>
 
-          <Image
-            src={session?.user?.image ?? ""}
-            alt="User profile"
-            width={32}
-            height={32}
-            className="rounded-full"
-          />
+            <Image
+              src={session?.user?.image ?? ""}
+              alt="User profile"
+              width={32}
+              height={32}
+              className="rounded-full"
+            />
 
-          <span className="font-sm font-inter text-[#1F384C]">
-            {session?.user?.name}
-          </span>
+            <span className="font-sm font-inter text-[#1F384C]">
+              {session?.user?.name}
+            </span>
 
-          {popoverOpen ? (
-            <ChevronIcon direction="down" />
-          ) : (
-            <ChevronIcon direction="up" />
-          )}
-        </div>
-      </PopoverTrigger>
+            {open ? (
+              <ChevronIcon direction="down" />
+            ) : (
+              <ChevronIcon direction="up" />
+            )}
+          </Menu.Button>
 
-      <PopoverContent align="end">
-        <Button
-          className="bg-green10 h-7 w-full justify-start gap-x-2 rounded text-black"
-          variant={"ghost"}
-          onClick={() => signOut()}
-        >
-          <UserIcon fill={false} size={16} />
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 z-10 mt-2.5 w-72 origin-top-right rounded-md bg-white p-4 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+              <Menu.Item>
+                <Button
+                  className="h-7 w-full justify-start gap-x-2 rounded bg-green10 text-black"
+                  variant={"ghost"}
+                  onClick={() => signOut()}
+                >
+                  <UserIcon fill={false} size={16} />
 
-          <span className="font-inter text-sm font-medium leading-4 text-black">
-            Cerrar sesión
-          </span>
-        </Button>
-      </PopoverContent>
-    </Popover>
+                  <span className="font-inter text-sm font-medium leading-4 text-black">
+                    Cerrar sesión
+                  </span>
+                </Button>
+              </Menu.Item>
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
+    </Menu>
   );
 };
 
