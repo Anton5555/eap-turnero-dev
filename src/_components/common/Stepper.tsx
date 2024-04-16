8cade11cb7ff72b1d0453c14c237890417783a4a
Anton5555 <antonmartinezq@gmail.com>
2024-04-16 16:05:39 -0300

    refactor: fixed stepper styling and functionallity

--- src/_components/common/Stepper.tsx
+++ src/_components/common/Stepper.tsx
@@ -16,7 +16,7 @@ const Stepper: React.FC<StepperProps> = ({
       {Array.from({ length: steps }, (_, i) => i + 1).map((step) => (
         <li
           key={step}
-          className={cn(step !== steps ? "pr-8 sm:pr-20" : "", "relative")}
+          className={cn(step !== steps ? "pr-20" : "", "relative")}
         >
           {step < currentStep ? (
             <>
@@ -24,10 +24,10 @@ const Stepper: React.FC<StepperProps> = ({
                 className="absolute inset-0 flex items-center"
                 aria-hidden="true"
               >
-                <div className="h-0.5 w-full bg-green" />
+                <div className="h-0.5 w-full bg-black" />
               </div>
 
-              <div className="relative flex h-8 w-8 items-center justify-center rounded-full bg-green">
+              <div className="relative flex h-8 w-8 items-center justify-center rounded-full bg-black">
                 <CheckIcon aria-hidden="true" />
 
                 <span className="sr-only">Step {step}</span>
@@ -43,11 +43,11 @@ const Stepper: React.FC<StepperProps> = ({
               </div>
 
               <div
-                className="relative flex h-8 w-8 items-center justify-center rounded-full border-2 border-green bg-white"
+                className="relative flex h-8 w-8 items-center justify-center rounded-full border-2 border-black bg-white"
                 aria-current="step"
               >
                 <span
-                  className="h-2.5 w-2.5 rounded-full bg-green"
+                  className="h-2.5 w-2.5 rounded-full bg-black"
                   aria-hidden="true"
                 />
               </div>
