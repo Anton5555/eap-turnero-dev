b07f2a11f3d3957b8144e36e9ef94ff78112bf55
Anton5555 <antonmartinezq@gmail.com>
2024-04-22 10:43:24 -0300

    refactor: moved user types to single file

---
+++ src/types/users.ts
@@ -0,0 +1,45 @@
+export type User = {
+  id: string;
+  email: string;
+  name: string;
+  lastName: string;
+  image: string;
+  company: number;
+  location: number;
+  userType: "employee" | "family";
+  services: string[];
+  position?: number;
+  accessToken: string;
+};
+
+type EAPUser = {
+  mail: string;
+  idpaciente: number;
+  sede: number;
+  pdp: string;
+  pdp_date: string;
+  fecha_nacimiento: string;
+  empresa: string;
+  empresaname: null | string;
+  telefono: null | string;
+  movil: null | string;
+  tipo: string;
+  img: null | string;
+  dni: null | string;
+  nombre: string;
+  apellido1: string;
+  apellido2: null | string;
+  huso: null | string;
+  tipousuarioportal: string;
+  a_b: string;
+  puesto: null | string;
+  turnoonline: string;
+  password: null | string;
+  paises: null | string;
+  services: Array<{ code: string; name: string }>;
+};
+
+export type AuthenticatedUser = {
+  user: EAPUser;
+  token: string;
+};
