2851da4aba0bf6e4f3124341fd0ee7a37a70c37e
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-04-23 17:16:24 -0300

    fix: return to h-full

--- src/app/platform/layout.tsx
+++ src/app/platform/layout.tsx
@@ -6,7 +6,7 @@ export const metadata = {
 };
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
-  <section className="h-screen w-screen bg-grayBg">
+  <section className="h-full w-screen bg-grayBg">
     <Sidebar>
       <div className="mx-auto lg:space-y-4 lg:px-8 lg:py-4 xl:max-w-screen-2xl">
         {children}
