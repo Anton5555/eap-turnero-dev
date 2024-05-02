ab7bef43d124394a4e22504d613f9577065f02aa
Anton5555 <antonmartinezq@gmail.com>
2024-05-02 10:18:53 -0300

    refactor: appointment creation calendar improved code and changed day variants styles

--- src/_components/appointments/Calendar.tsx
+++ src/_components/appointments/Calendar.tsx
@@ -1,199 +1,95 @@
 "use client";
 
 import React from "react";
-import {
-  CaptionLabelProps,
-  CaptionProps,
-  DayPicker,
-  MonthChangeEventHandler,
-  useDayPicker,
-  useNavigation,
-} from "react-day-picker";
+import { DayPicker, DayPickerSingleProps } from "react-day-picker";
 import cn from "~/lib/utils";
 import { buttonVariants } from "../common/Button";
 import ChevronLeftIcon from "../icons/ChevronLeft";
 import ChevronRightIcon from "../icons/ChevronRight";
-import { Select } from "../common/Select";
 import { es } from "date-fns/locale";
-import Link from "next/link";
+import { format } from "date-fns";
 
 const currentDay = new Date();
 const currentMonth = currentDay.getMonth();
 const currentYear = currentDay.getFullYear();
 
-// TODO: Implement a better way to handle months in the caption component
-const months = [
-  { value: 0, label: "Enero" },
-  { value: 1, label: "Febrero" },
-  { value: 2, label: "Marzo" },
-  { value: 3, label: "Abril" },
-  { value: 4, label: "Mayo" },
-  { value: 5, label: "Junio" },
-  { value: 6, label: "Julio" },
-  { value: 7, label: "Agosto" },
-  { value: 8, label: "Septiembre" },
-  { value: 9, label: "Octubre" },
-  { value: 10, label: "Noviembre" },
-  { value: 11, label: "Diciembre" },
-] as const;
-
-const years = [{ value: currentYear }, { value: currentYear + 1 }];
-
-const CaptionLabel: React.FC<
-  CaptionLabelProps & {
-    onYearClick: (year: string) => void;
-    onMonthClick: (month: string) => void;
-  }
-> = (props) => {
-  const { classNames } = useDayPicker();
-  const { displayMonth, onYearClick, onMonthClick } = props;
-
-  return (
-    <div
-      key="caption"
-      className={classNames.caption_label}
-      aria-live="polite"
-      aria-atomic="true"
-    >
-      <Select
-        options={months.map((month) => ({
-          ...month,
-          disabled:
-            displayMonth.getFullYear() === currentYear &&
-            currentDay.getMonth() > month.value,
-        }))}
-        className="bg-ultra-light-gray h-9 w-3/4 rounded-lg pl-3 pr-0 text-sm font-bold text-green ring-0 lg:h-10"
-        onChange={(e) => onMonthClick(e.target.value)}
-      />
-
-      <Select
-        options={years}
-        className="bg-ultra-light-gray -ml-5 h-9 w-7/12 rounded-lg pl-2 pr-0 text-sm font-bold text-green ring-0 lg:h-10"
-        onChange={(e) => onYearClick(e.target.value)}
-      />
-
-      <div className="flex items-center lg:hidden">
-        <Link
-          href="#"
-          className="mr-2"
-          onClick={() => onMonthClick((displayMonth.getMonth() - 1).toString())}
-        >
-          <ChevronLeftIcon />
-        </Link>
-
-        <Link
-          href="#"
-          className="ml-4"
-          onClick={() => onMonthClick((displayMonth.getMonth() + 1).toString())}
-        >
-          <ChevronRightIcon />
-        </Link>
-      </div>
-    </div>
-  );
-};
-
-const CustomCaption = (props: CaptionProps) => {
-  const { displayMonth } = props;
-  const context = useDayPicker();
-  const { classNames, styles, onMonthChange } = context;
-
-  const { goToMonth } = useNavigation();
-
-  const onYearClick = (year: string) =>
-    handleMonthChange(
-      new Date(
-        Number(year),
-        Number(year) === currentYear && displayMonth.getMonth() < currentMonth
-          ? currentMonth
-          : displayMonth.getMonth(),
-      ),
-    );
-
-  const onMonthClick = (month: string) =>
-    handleMonthChange(new Date(displayMonth.getFullYear(), Number(month)));
-
-  const handleMonthChange: MonthChangeEventHandler = (newMonth) => {
-    goToMonth(newMonth);
-    onMonthChange?.(newMonth);
-  };
-
-  return (
-    <div className={classNames.caption} style={styles.caption}>
-      <CaptionLabel
-        displayMonth={displayMonth}
-        onYearClick={onYearClick}
-        onMonthClick={onMonthClick}
-      />
-    </div>
-  );
-};
-
-type CalendarProps = React.ComponentProps<typeof DayPicker> & {
+type CalendarProps = DayPickerSingleProps & {
   availableDays: string[];
 };
 
 const SUNDAY = 0;
 
+const calendarClassNames = {
+  months:
+    "flex justify-center space-y-4 sm:space-x-4 sm:space-y-0 pb-4 lg:pb-0",
+  month: "space-y-4",
+  caption_label: "hidden",
+  nav: "space-x-4 lg:hidden flex items-center",
+  caption:
+    "flex justify-between lg:justify-center pt-4 lg:pt-1 px-2 lg:px-0 items-center",
+  caption_dropdowns: "flex gap-2",
+  dropdown:
+    "flex bg-ultra-light-gray border-none h-9 w-full rounded-lg pl-3 pr-0 text-sm font-bold text-green ring-0 lg:h-10 focus:ring-green focus:ring-2 focus:ring-inset focus:border-none",
+  dropdown_month: "w-32",
+  dropdown_year: "w-20",
+  table: "w-full max-w-none border-collapse space-y-1",
+  head_row:
+    "flex border-b-[0.5px] border-b-light-gray pb-2 lg:border-none lg:hidden justify-between ",
+  head_cell:
+    "lg:text-dark-gray text-black font-semibold capitalize w-12 md:w-16 lg:w-16 xl:w-24 2xl:w-28 lg:font-bold",
+  row: "flex w-full mt-2 justify-between",
+  cell: "h-12 w-12 lg:h-10 md:w-16 lg:w-16 xl:w-24 2xl:w-28 pointer-events-none cursor-default text-center lg:text-base font-normal p-0 relative [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent focus-within:relative focus-within:z-20",
+  day: cn(
+    buttonVariants({ variant: "ghost" }),
+    "h-12 w-12 lg:h-10 md:w-16 lg:w-16 xl:w-24 2xl:w-28 p-0 font-normal mx-auto aria-selected:opacity-100 hover:text-black",
+  ),
+  day_outside:
+    "opacity-50 aria-selected:bg-green/50 aria-selected:opacity-30 pointer-events-none cursor-default",
+  day_disabled:
+    "opacity-50 hover:bg-transparent hover:opacity-50 hover:text-black pointer-events-none cursor-default",
+  day_hidden: "invisible",
+};
+
 const Calendar: React.FC<CalendarProps> = ({
   availableDays,
   className,
-  classNames,
   showOutsideDays = true,
-  onDayClick,
   onMonthChange,
+  selected,
   ...props
 }) => (
   <DayPicker
     locale={es}
     weekStartsOn={SUNDAY}
     disabled={(date) => date.getDate() < currentDay.getDate()}
-    onDayClick={onDayClick}
     onMonthChange={onMonthChange}
+    defaultMonth={currentDay}
+    fromMonth={currentDay}
+    toYear={currentYear + 1}
+    selected={selected}
     modifiers={{
-      available: availableDays.map(
-        (day) => new Date(currentYear, currentMonth, Number(day)),
-      ),
+      available: availableDays
+        .filter((day) => Number(day) !== selected?.getDate())
+        .map((day) => new Date(currentYear, currentMonth, Number(day))),
     }}
     modifiersClassNames={{
-      available: "bg-green text-white cursor-pointer pointer-events-auto",
+      available:
+        "bg-light-gray text-black hover:bg-green/50 cursor-pointer pointer-events-auto",
+      selected: "bg-green text-white",
+    }}
+    formatters={{
+      formatMonthCaption: (date) => {
+        const formattedDate = format(date, "LLLL", { locale: es });
+        return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+      },
     }}
     showOutsideDays={showOutsideDays}
     captionLayout="dropdown-buttons"
-    fromYear={currentYear}
-    toYear={currentYear + 1}
     className={cn("font-openSans text-base text-black", className)}
-    classNames={{
-      months:
-        "flex justify-center space-y-4 sm:space-x-4 sm:space-y-0 pb-4 lg:pb-0",
-      month: "space-y-4",
-      caption:
-        "flex justify-between lg:justify-center pt-4 lg:pt-1 px-2 lg:px-0 items-center",
-      caption_label: "flex flex-row justify-between lg:justify-center",
-      table: "w-full max-w-none border-collapse space-y-1",
-      head_row:
-        "flex border-b-[0.5px] border-b-light-gray pb-2 lg:border-none lg:hidden justify-between ",
-      head_cell:
-        "lg:text-dark-gray text-black font-semibold capitalize w-12 md:w-16 lg:w-16 xl:w-24 2xl:w-28 lg:font-bold",
-      row: "flex w-full mt-2 justify-between",
-      cell: "h-12 w-12 lg:h-10 md:w-16 lg:w-16 xl:w-24 2xl:w-28 pointer-events-none cursor-default text-center lg:text-base font-normal p-0 relative [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent focus-within:relative focus-within:z-20",
-      day: cn(
-        buttonVariants({ variant: "ghost" }),
-        "h-12 w-12 lg:h-10 md:w-16 lg:w-16 xl:w-24 2xl:w-28 p-0 font-normal mx-auto aria-selected:opacity-100 hover:bg-transparent hover:text-black",
-      ),
-      day_selected:
-        "bg-green/50 text-white hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-      day_outside:
-        "opacity-50 aria-selected:bg-green/50 aria-selected:opacity-30 pointer-events-none cursor-default",
-      day_disabled:
-        "opacity-50 hover:bg-transparent hover:opacity-50 hover:text-black pointer-events-none cursor-default",
-      day_hidden: "invisible",
-      ...classNames,
-    }}
+    classNames={calendarClassNames}
     components={{
       IconLeft: () => <ChevronLeftIcon />,
       IconRight: () => <ChevronRightIcon />,
-      Caption: CustomCaption,
     }}
     {...props}
   />
