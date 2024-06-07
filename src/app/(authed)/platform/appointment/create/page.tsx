ed0ff71935a509b4e8fbadfafc96381833d1336f
Anton5555 <antonmartinezq@gmail.com>
2024-06-07 08:24:10 -0300

    feat: removed filtered services

--- src/app/(authed)/platform/appointment/create/page.tsx
+++ src/app/(authed)/platform/appointment/create/page.tsx
@@ -2,7 +2,6 @@ import { getServerSession } from "next-auth";
 import CreateAppointment from "~/_components/appointments/CreateAppointment";
 import { getContractServices } from "~/lib/api/services";
 import authOptions from "~/lib/authOptions";
-import { SPECIALTY } from "~/types/services";
 
 const Page = async () => {
   const session = await getServerSession(authOptions);
@@ -13,18 +12,12 @@ const Page = async () => {
 
   const { accessToken, company, location, position } = user;
 
-  const services = (
-    await getContractServices({
-      companyId: company,
-      locationId: location,
-      positionId: position ?? -1,
-      accessToken: accessToken,
-    })
-  ).filter(
-    (service) =>
-      service.specialty === SPECIALTY.PSICOLOGY ||
-      service.specialty === SPECIALTY.NUTRITION,
-  );
+  const services = await getContractServices({
+    companyId: company,
+    locationId: location,
+    positionId: position ?? -1,
+    accessToken: accessToken,
+  });
 
   return <CreateAppointment services={services} user={user} />;
 };
