380c6cc2ee8002d9a972c0957a70cf5cbc2a74dc
Anton5555 <antonmartinezq@gmail.com>
2024-06-12 12:27:04 -0300

    feat: Added radix accordion (shadcn)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,6 +35,20 @@ export default {
         "gradient-linear":
           "linear-gradient(180.81deg, rgba(217, 217, 217, 0) 24.75%, #009D7D 123.9%)",
       }),
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [require("@tailwindcss/forms")],
