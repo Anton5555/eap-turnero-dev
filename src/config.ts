99883c35299a290d71929b2d643878059c29a19f
Anton5555 <antonmartinezq@gmail.com>
2024-06-19 15:21:27 -0300

    chore: simplified locale switcher logic + changed pathnames type in next-intl create pathnames declaration

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,4 @@
-import { type Pathnames } from "next-intl/navigation";
-
 export const defaultLocale = "es";
 export const locales = ["en", "es"] as const;
-
-export const pathnames = {
-  "/": "/",
-  "/pathnames": {
-    en: "/pathnames",
-    es: "/rutas",
-  },
-} satisfies Pathnames<typeof locales>;
 
 export const localePrefix = undefined;
